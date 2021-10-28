10b320b2c6c3598a2e59fd7129f6141cc1b34d2e
Jakub Mokrzak <jakub.mokrzak@gmail.com>
2021-10-28 12:08:37 +0200

    Final version.

---
+++ zad2.py
@@ -0,0 +1,12 @@
+#Napisz funkcję, która dla zadanej listy elementów pobierze od użytkownika liczbę k i zwróci k-ty element od końca listy. 
+#Uwaga: załóż, że użytkownik podaje poprawne wartości (tzn. 0 < k <= długość listy).
+
+
+def kth(k):
+    n = int(input("Enter the number: "))
+    elemens = list(range(1, n+1))
+    return elemens[-k]
+
+print(kth(3))
+
+
